66b9823ab0a5804ffc6db7817660560829caaa27
MomoOnCode <kylemuell@proton.me>
2026-07-16 21:42:01 -0500

    had some errors in the query structure and the FoundItems class didnt have a date field so it was never propagating to the frontend through json

--- src/main/java/mavsreclaim/FoundItem.java
+++ src/main/java/mavsreclaim/FoundItem.java
@@ -2,4 +2,4 @@ package mavsreclaim;
 
  
 public record FoundItem(int id, String description, String category, String building,
-                        String finderEmail, Integer lockerId, String pin, String status) {}
+                        String finderEmail, Integer lockerId, String pin, String status, String createdAt) {}
