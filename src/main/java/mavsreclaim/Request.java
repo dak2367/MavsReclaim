15b2a09ae02ca99527b8bea3efbfa8977353f025
David Kim <dak2367@mavs.uta.edu>
2026-07-16 22:20:21 -0500

    added request class and updated found item class. added backend routes for both

--- src/main/java/mavsreclaim/Request.java
+++ src/main/java/mavsreclaim/Request.java
@@ -1,35 +1,34 @@
 package mavsreclaim;
 
-public class FoundItem 
+public class Request
 {
     private int id;
     private String description;
     private String category;
     private String building;
-    private String finderEmail;
-    private Integer lockerId;
-    private String pin;
+    private String lostOn;
+    private String requesterEmail;
     private String status;
+    private int matchedItemId;
 
-    //Found Item object for building form
-    public FoundItem(int id, String description, String category, String building,String finderEmail, Integer lockerId, String pin, String status) 
+    public Request(int id, String description, String category, String building, String lostOn, String requesterEmail, String status, int matchedItemId)
     {
         this.id = id;
         this.description = description;
         this.category = category;
         this.building = building;
-        this.finderEmail = finderEmail;
-        this.lockerId = lockerId;
-        this.pin = pin;
+        this.lostOn = lostOn;
+        this.requesterEmail = requesterEmail;
         this.status = status;
+        this.matchedItemId = matchedItemId;
     }
 
     public int id() { return id; }
     public String description() { return description; }
     public String category() { return category; }
     public String building() { return building; }
-    public String finderEmail() { return finderEmail; }
-    public Integer lockerId() { return lockerId; }
-    public String pin() { return pin; }
+    public String lostOn() { return lostOn; }
+    public String requesterEmail() { return requesterEmail; }
     public String status() { return status; }
+    public Integer matchedItemId() { return matchedItemId; }
 }
